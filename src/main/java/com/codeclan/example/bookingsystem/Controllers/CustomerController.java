da5871a3e20d05f420be621adbe9cfa398e37e37
chloe alcorn <ca.alcorn@outlook.com>
2018-10-25 15:08:25 +0100

    path added for get customers on course

---
+++ src/main/java/com/codeclan/example/bookingsystem/Controllers/CustomerController.java
@@ -0,0 +1,25 @@
+package com.codeclan.example.bookingsystem.Controllers;
+
+
+import com.codeclan.example.bookingsystem.Repositories.CustomerRepository.CustomerRepository;
+import com.codeclan.example.bookingsystem.models.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/customers")
+public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/course/{id}")
+    public List<Customer> getCustomerOnCourse(@PathVariable Long id){
+        return  customerRepository.getCustomersOnCourse(id);
+    }
+}
