e645b634b983b4c34ec693a03d7f56fa18cfb3f6
chloe alcorn <ca.alcorn@outlook.com>
2018-10-25 11:00:52 +0100

    course model created

---
+++ src/main/java/com/codeclan/example/bookingsystem/models/Course.java
@@ -0,0 +1,51 @@
+package com.codeclan.example.bookingsystem.models;
+
+public class Course {
+
+    private Long id;
+    private String courseName;
+    private String location;
+    private CourseRating rating;
+
+    public Course(String courseName, String location, CourseRating rating) {
+        this.courseName = courseName;
+        this.location = location;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public CourseRating getRating() {
+        return rating;
+    }
+
+    public void setRating(CourseRating rating) {
+        this.rating = rating;
+    }
+
+    public int getRatingValue(){
+        return this.rating.getRating();
+    }
+}
---
+++ src/main/java/com/codeclan/example/bookingsystem/models/CourseRating.java
@@ -0,0 +1,21 @@
+package com.codeclan.example.bookingsystem.models;
+
+public enum CourseRating {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5);
+
+
+    private final int rating;
+    CourseRating(int rating){
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+}
+
+
